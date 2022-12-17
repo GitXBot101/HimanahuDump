fab83cfa9baa57e11a6bab1ddd361e5c7ec97b51
SdWrkflow <119104619+SdWrkflow@users.noreply.github.com>
2022-12-17 12:50:06 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -52,10 +52,10 @@ except:
 
 if tg_session:
   LOG.info("Session Mode - {tg_session}")
-  app = Client(tg_session, api_id, api_hash, ipv6=True)
+  app = Client(tg_session, api_id, api_hash)
 elif bot_token:
   LOG.info("Bot Mode")
-  app = Client(":memory:", api_id, api_hash, bot_token=bot_token, ipv6=True)
+  app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
 else:
   LOG.error("Set either TELEGRAM_SESSION or BOT_TOKEN variable.")
   sys.exit(1)
